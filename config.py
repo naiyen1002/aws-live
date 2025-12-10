8f04fc48201fa5bd90918667b85c18eeac9de8b3
Yenny <110795520+naiyen1002@users.noreply.github.com>
2025-12-10 15:18:26 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
-customhost = "database-1.chauwtk5goof.us-east-1.rds.amazonaws.com"
+customhost = "employee.cp2m60uu24l4.us-east-1.rds.amazonaws.com"
 customuser = "aws_user"
 custompass = "Bait3273"
 customdb = "employee"
-custombucket = "lowchoonkeat-employee1"
+custombucket = "kokny-employee"
 customregion = "us-east-1"
 
